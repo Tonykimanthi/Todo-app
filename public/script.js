ac06f8d5d162e820816ccca9863fb53d693bf44d
Tony Km <kimaxhanthony9@gmail.com>
2023-05-11 19:36:05 +0300

    Added save functionality

--- public/script.js
+++ public/script.js
@@ -21,9 +21,10 @@ form.addEventListener("submit", function (e) {
 
   // Delete button
   const deleteBtn = document.createElement("span");
-  deleteBtn.innerText = "Delete";
+  deleteBtn.innerHTML = "Delete";
   deleteBtn.style.color = "white";
-  deleteBtn.style.backgroundColor = "red";
+  deleteBtn.style.fontSize = "1.1rem";
+  deleteBtn.style.backgroundColor = "#e84118";
   deleteBtn.style.borderRadius = ".2em";
   deleteBtn.style.padding = ".2em";
   deleteBtn.style.cursor = "pointer";
@@ -42,5 +43,26 @@ form.addEventListener("submit", function (e) {
 //   Remove todo
   deleteBtn.addEventListener("click", function(e){
     e.target.parentElement.remove();
-  })
+  });
+
+
+//   Strike through on a todo
+newList.addEventListener("dblclick", function(){
+    if(todoText.style.textDecoration === "none"){
+    newList.style.opacity = ".5";
+    deleteBtn.innerText = "Saved";
+    deleteBtn.style.backgroundColor = "green";
+    deleteBtn.style.pointerEvents = "none";
+    todoText.style.textDecoration = "line-through";
+    }else {
+        newList.style.opacity = "1";
+        deleteBtn.innerText = "Delete";
+        deleteBtn.style.backgroundColor = "red";
+        deleteBtn.style.pointerEvents = "auto";
+        todoText.style.textDecoration = "none";
+    }
+
+    
+});
+
 });
